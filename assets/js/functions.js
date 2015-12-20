410251a0ffa781559b8a00cbdf436b2d36bbb9dd
derek.morash <derek.morash@gmail.com>
2015-12-20 00:54:04 -0400

    about section positioned to center of window using js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,11 +1,11 @@
 $(function() {
   smoothScroll(300);
-  //initialize fit text
-  // $('#email').fitText(1, { minFontSize: '10px', maxFontSize: '100px' });
 
   hugeText('footer--email','email');
 
   $('#email').fluidText();
+
+  headerPosition();
 });
 
 // smoothScroll function is applied from the document ready function
@@ -23,12 +23,41 @@ function smoothScroll (duration) {
 	});
 }
 
+//about section margin-top to position header in the middle of the page
+function headerPosition() {
+  var about = $('.about'),
+    windowHeight;
+
+  reposition();
+
+  function reposition() {
+    //get the height of the window
+    windowHeight = $(window).height();
+
+    //if the window is taller than 400px
+    if(windowHeight > 400) {
+      //add a margin-top to the about selection-color
+      //margin is half the window height minus the 100px header
+      about.css('margin-top', (windowHeight/2) - 100);
+    } else {
+      //usually for phones in landscape
+      //add margin-top of only 50px
+      about.css('margin-top', 50);
+    }
+  }
+
+  //call the reposition function on window resize
+  $( window ).resize(function() {
+    reposition();
+  });
+}
+
 //Text resizing
 function hugeText (p, t) {
   var parent = $('.'+p); //get parent
   var text = $('#'+t);   //get text
 
-  alert(parent.width() + ' and ' + text.width());
+  // alert(parent.width() + ' and ' + text.width());
 
   size();
 
@@ -51,8 +80,8 @@ function hugeText (p, t) {
     var parent = $(this);
     // alert(this.html());
     // alert(this.outerWidth());
-    alert(parent.html());
-    alert(parent.width());
+    // alert(parent.html());
+    // alert(parent.width());
     return this.each(function(){
 
     });
